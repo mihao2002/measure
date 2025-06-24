df5c4cac7f4e4dabb246df471c2f1571d587cd2c
Hao Mi <mihao_2002@hotmail.com>
2025-06-24 15:17:39 -0700

    Initial Commit

---
+++ measure/measureApp.swift
@@ -0,0 +1,17 @@
+//
+//  measureApp.swift
+//  measure
+//
+//  Created by Hao Mi on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct measureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
